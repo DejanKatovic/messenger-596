5306b329f9fa64628b7466661f26b90869088f0b
DejanKatovic <88758044+DejanKatovic@users.noreply.github.com>
2021-09-09 12:56:03 -0400

    #3 Updated

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,27 +2,20 @@ const Conversation = require("./conversation");
 const User = require("./user");
 const Message = require("./message");
 const Channel = require("./channel");
-const ChannelUser = require("./channelUser");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, {through: Channel});
+Conversation.belongsToMany(User, {through: Channel});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 Message.belongsTo(Channel);
 Channel.hasMany(Message);
 Message.belongsTo(Channel);
-Channel.hasMany(ChannelUser);
-ChannelUser.belongsTo(Channel);
-User.hasMany(ChannelUser);
-ChannelUser.belongsTo(User);
 
 module.exports = {
   User,
   Conversation,
   Message,
-  Channel,
-  ChannelUser
+  Channel
 };
